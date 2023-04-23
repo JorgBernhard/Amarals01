af12fe5a2a02c3616aa463b4781148371d8ec2ca
Jorg Bernhard Reske <93399055+JorgBernhard@users.noreply.github.com>
2023-04-23 13:44:33 -0300

    update restaurant

--- src/pages/Restaurant.js
+++ src/pages/Restaurant.js
@@ -7,11 +7,6 @@ const Restaurant = () => {
     return <>
     <div className="Restaurant">
         <div className='RestaurantDesign'>
-            <div className='Texto'>
-              <h1 className='BemVindo'>Willkommen bei</h1>
-              <h1 className='Amarals'>Amarals Internationalen Küche</h1>
-            </div>
-        </div>
             <div className='Boden'>
               <div className='ZeitenLinks'>
                 <h3 className='Offen'>Unsere Öffnungszeiten sind von:<br></br><br></br>
@@ -20,13 +15,13 @@ const Restaurant = () => {
                 <Link className="RestaurantReservierungen" to="/reservierungen">Reservierungen</Link>
                 <Outlet />
               </div>
-              <div className='AdresseRechts'>
-                <h2 className='RestaurantAdresse'>Amarals Restaurant</h2>
-                <h3 className='Adresse'>
-                Ahornstraße, 7 / 74363 Güglingen<br></br><br></br>
-                Telefon: 07135 9360543 / 0176 35882171</h3>
-              </div>
             </div>
+            
+            <div className='Texto'>
+              <h1 className='BemVindo'>Willkommen bei</h1>
+              <h1 className='Amarals'>Amarals Internationalen Küche</h1>
+            </div>
+          </div>
     </div>         
     </>
   };
